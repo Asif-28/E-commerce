131ba0bf6dc3d21e19a28328efbe547dac163189
Asif Ali <asif.a.7277@gmail.com>
2022-06-20 12:54:09 +0530

    Started E-Commerce/navbar/slider

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
+import { Badge } from "@mui/material";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+const Container = styled.div`
+  height: 60px;
+`;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const Language = styled.div`
+  font: 14px;
+  cursor: pointer;
+`;
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgrey;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+const Input = styled.input`
+  border: none;
+`;
+const Centre = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+const MenuItems = styled.div`
+  margin-left: 25px;
+`;
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <SearchIcon style={{ color: "grey", fontSize: "16px" }} />
+          </SearchContainer>
+        </Left>
+        <Centre>
+          <Logo>Sold.</Logo>
+        </Centre>
+        <Right>
+          <MenuItems>REGISTER</MenuItems>
+          <MenuItems>LOG IN</MenuItems>
+          <MenuItems>
+            <Badge badgeContent={0} color="secondary"></Badge>
+            <ShoppingCartOutlinedIcon />
+          </MenuItems>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
